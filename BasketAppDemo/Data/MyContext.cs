2ac901f7ec0ac1991d6044a8923e3cb898749ad5
hkpln <h.kpln0681@gmail.com>
2022-04-19 00:33:57 +0300

    Add project files.

---
+++ BasketAppDemo/Data/MyContext.cs
@@ -0,0 +1,15 @@
+﻿using BasketAppDemo.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BasketAppDemo.Data
+{
+    public class MyContext : DbContext
+    {
+        public MyContext(DbContextOptions<MyContext> options)
+            : base(options)
+        {
+
+        }
+        public DbSet<Product> Products { get; set; }
+    }
+}
